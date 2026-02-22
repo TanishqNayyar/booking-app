353f9b40489b3fbb7ee1f8f0f1a0674740a381e4
Tanishq nayyar <tanishqnayyar786@gmail.com>
2026-02-22 22:34:08 +0530

    backend production fixes

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,34 @@ const apiRoutes = require('./routes/api');
 const app = express();
 const server = http.createServer(app);
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://booking-88dor2d4t-tanishq-nayyars-projects.vercel.app"
+];
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json());
 
 // Socket.io Setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
+
   socket.on('join_expert', (expertId) => {
     socket.join(`expert_${expertId}`);
   });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
@@ -36,12 +47,8 @@ app.set('io', io);
 
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
-  .then(async () => {
+  .then(() => {
     console.log('MongoDB Connected');
-    
-    // Seed experts
-    const expertController = require('./controllers/expertController');
-    await expertController.seedExperts();
   })
   .catch(err => console.log(err));
 
